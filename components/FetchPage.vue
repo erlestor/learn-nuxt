e1dcb4ad6a80ad120842d3be09927f89cb23ef66
Erlend Storsve <erlestor@ntnu.no>
2023-05-03 18:07:25 +0200

    add template for fetch pages

---
+++ components/FetchPage.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const { header } = defineProps(["header"])
+
+const { data, pending, refresh } = await useLazyFetch("/api/getCurrentTime", {
+  transform: (data) => data.substring(0, 19),
+})
+</script>
+
+<template>
+  <Page>
+    <span class="text-h2 text-secondary">{{ header }}</span>
+    <span class="text-h4" v-if="data">Last fetch at {{ data }}</span>
+    <v-progress-circular
+      v-if="pending && !data"
+      indeterminate
+      :size="40"
+    ></v-progress-circular>
+    <v-btn
+      color="primary"
+      :loading="pending && data"
+      :disabled="pending"
+      @click="refresh"
+      >Refresh</v-btn
+    >
+    <NuxtLink to="/fetch">
+      <v-btn>Back</v-btn>
+    </NuxtLink>
+  </Page>
+</template>
