fc265307a6a1dd18da0f4ca64c9b41c39854435c
Erlend Storsve <erlestor@ntnu.no>
2023-05-03 21:03:24 +0200

    chore: move fetch page

---
+++ pages/fetch/index.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const pages = ["client", "static", "server"]
+</script>
+
+<template>
+  <Page>
+    <span class="text-h1 text-secondary">Fetch</span>
+    <span class="text-body1"
+      >Click these buttons to test different rendering patterns</span
+    >
+    <!-- TODO: use buttongroup if it exists -->
+    <div class="flex">
+      <div v-for="page in pages" :key="page">
+        <NuxtLink :to="`/fetch/${page}`">
+          <v-btn>{{ page }}</v-btn>
+        </NuxtLink>
+      </div>
+    </div>
+  </Page>
+</template>
+
+<style scoped>
+.flex {
+  display: flex;
+}
+</style>
