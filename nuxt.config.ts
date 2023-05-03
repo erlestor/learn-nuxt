93f16269bbd41ebdbd5d540b6f5072f9118b0f24
Erlend Storsve <erlestor@ntnu.no>
2023-05-03 04:47:31 +0200

    fix comment out eslint plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@invictus.codes/nuxt-vuetify", "@nuxtjs/eslint-module"],
+  modules: [
+    "@invictus.codes/nuxt-vuetify",
+    // cause eslint sucks apparently
+    // ["@nuxtjs/eslint-module", { fix: true }],
+  ],
   vuetify: {
     vuetifyOptions: {
       theme: {
