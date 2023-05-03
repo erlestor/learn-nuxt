211d283d1b48317f3ebb737d63e5c2df4b93ed4f
Erlend Storsve <erlestor@ntnu.no>
2023-05-03 04:48:05 +0200

    feat basic pages

---
+++ pages/about.vue
@@ -0,0 +1,21 @@
+<script lang="ts"></script>
+
+<template>
+  <main>
+    <span class="text-h1 text-secondary">About</span>
+    <NuxtLink to="/">
+      <v-btn color="primary">Go home</v-btn>
+    </NuxtLink>
+  </main>
+</template>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 24px;
+  height: 100vh;
+  gap: 16px;
+}
+</style>
