0e2437da4ad1ad2257a987a242ecbfb85b0ece18
Erlend Storsve <erlestor@ntnu.no>
2023-05-03 04:47:59 +0200

    feat basic pages

--- pages/index.vue
+++ pages/index.vue
@@ -2,15 +2,20 @@
 
 <template>
   <main>
-    <v-btn color="primary">Home</v-btn>
+    <span class="text-h1 text-secondary">Home</span>
+    <NuxtLink to="/about">
+      <v-btn color="primary">About</v-btn>
+    </NuxtLink>
   </main>
 </template>
 
 <style scoped>
 main {
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
+  padding-top: 24px;
   height: 100vh;
+  gap: 16px;
 }
 </style>
