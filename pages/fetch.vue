50d938f2fcd6d8088a722ab764620eef55d2a246
Erlend Storsve <erlestor@ntnu.no>
2023-05-03 08:05:35 +0200

    add fetch page

---
+++ pages/fetch.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const { data } = await useFetch("/api/hello", {
+  transform: (data) => data.substring(0, 19),
+})
+</script>
+
+<template>
+  <div class="page">
+    <span class="text-h1 text-secondary">Fetch</span>
+    <span class="text-h3">Data fetched at {{ data }}</span>
+  </div>
+</template>
+
+<style scoped>
+.page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 24px;
+  gap: 16px;
+}
+</style>
