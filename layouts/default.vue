883c56e8d6de60e2cde7b5b61d5da80ff587a63d
Erlend Storsve <erlestor@ntnu.no>
2023-05-03 08:04:55 +0200

    add default layout with navigation

---
+++ layouts/default.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+const drawer = useState(() => false)
+
+const navLinks = [
+  { title: "Home", link: "/" },
+  { title: "Fetch", link: "/fetch" },
+]
+</script>
+
+<template>
+  <v-layout>
+    <v-app-bar>
+      <v-app-bar-nav-icon
+        variant="text"
+        @click.stop="drawer = !drawer"
+      ></v-app-bar-nav-icon>
+      <v-toolbar-title>Learn Nuxt</v-toolbar-title>
+      <v-spacer />
+      <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer">
+      <v-list>
+        <div v-for="{ link, title } in navLinks">
+          <NuxtLink :to="link">
+            <v-list-item :title="title" />
+          </NuxtLink>
+        </div>
+      </v-list>
+    </v-navigation-drawer>
+    <v-main>
+      <slot />
+    </v-main>
+  </v-layout>
+</template>
+
+<style scoped>
+a {
+  text-decoration: none;
+  color: inherit;
+}
+</style>
